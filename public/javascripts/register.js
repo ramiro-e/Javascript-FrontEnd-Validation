d27dabd062bc84b79db668d5e52876685ebe3777
ramiro-e <ramiroestevez96@gmail.com>
2021-10-29 23:11:20 -0300

    Validacion de register completa

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,13 +1,13 @@
 let form = document.querySelector('form')
-let nombre = document.getElementById('nombre');
-let apellido = document.getElementById('apellido')
-let dia = document.getElementById('dia')
-let mes = document.getElementById('mes')
-let año = document.getElementById('año')
-let email = document.getElementById('email')
-let contraseña = document.getElementById('contraseña')
-let repetirContraseña = document.getElementById('repetirContraseña');
-let aceptarTyC = document.getElementById('aceptarTyC');
+let nombre = document.querySelector('input#nombre');
+let apellido = document.querySelector('input#apellido')
+let dia = document.querySelector('select#dia')
+let mes = document.querySelector('select#mes')
+let año = document.querySelector('select#año')
+let email = document.querySelector('input#email')
+let contraseña = document.querySelector('input#contraseña')
+let repetirContraseña = document.querySelector('input#repetirContraseña');
+let aceptarTyC = document.querySelector('input#aceptarTyC');
 
 let soloLetrasRGEX = /^[a-zA-Z\s]*$/;
 let diaRGEX = /^\b(0?[1-9]|[12][0-9]|3[01])\b/;
@@ -21,6 +21,7 @@ let soloLetrasError = "El campo solo puede contener letras"
 let emailError = "El campo no contiene un email valido"
 let contraseñaError = "El campo debe contener letras mayusculas, minusculas y numeros "
 let repetirContraseñaError = "Las contraseñas no coinciden"
+let aceptarTyCError = "Debe aceptar los terminos y condiciones"
 
 function setErrorClass(input){
     input.classList.remove("input-success")
@@ -61,7 +62,8 @@ function focusInput(input){
 }
 
 function blurTextInput(input, error, RGEX){
-    let errorMsg = input.parentElement.children[2]
+    let errorMsgId = "small#" + input.id
+    let errorMsg = document.querySelector(errorMsgId);
     input.classList.remove("input-neutral-selected")
     input.classList.remove("input-error-selected")
     if(input.value === ""){
@@ -80,60 +82,91 @@ function blurTextInput(input, error, RGEX){
 }
 
 function blurDateInput(input){
-    let errorMsg = document.getElementById('dateErrorMsg');
-    let error = "El campo " + input.id + " esta vacio"
+    let errorMsg = document.querySelector('small#date')
     input.classList.remove("input-neutral-selected")
     input.classList.remove("input-error-selected")
-    if (input.value === "") {
-        setErrorClass(input)
+    let dates = [dia,mes,año]
+    let errorDates = []
+    let datePosition
+    dates.forEach((date, index)=>{
+        if (input.id === dates[index].id) {
+            datePosition = index
+        }
+    })
+    for (let i = 0; i <= datePosition; i++) {
+        let date = dates[i]
+        if(date.value === ""){
+            if (!errorDates.length){
+                errorDates = errorDates + date.id
+            }else{
+                errorDates = errorDates + ", " + date.id
+            }
+            setErrorClass(date)
+        }else{
+            setSuccessClass(date)
+        }
+    }
+    let error = "El campo " + errorDates + " esta vacio"
+    if(errorDates.length > 0){
         setErrorMsg(errorMsg ,error)
         return false
     }else{
-        setSuccessClass(input)
+        setSuccessMsg(errorMsg)
+        return true
+    }   
+}
+
+// function submitDateCheck(){
+//     let errorMsg = document.querySelector('date');
+//     let dates = [dia,mes,año]
+//     let errorDates = []
+//     dates.forEach((date)=>{
+//         if(date.value === ""){
+//             if (!errorDates.length){
+//                 errorDates = errorDates + date.id
+//             }else{
+//                 errorDates = errorDates + ", " + date.id
+//             }
+//         }
+//     })
+
+//     let error = "El campo " + errorDates + " esta vacio"
+//     if(!errorDates.length){
+//         setErrorMsg(errorMsg ,error)
+//     }else{
+//         setSuccessMsg(errorMsg)
+//     }
+// }
+
+function submitCheckboxCheck(input, error){
+    let errorMsg = document.querySelector('small#aceptarTyC');
+    console.log(errorMsg)
+    if(!input.checked){
+        setErrorMsg(errorMsg , error)
+        return false
+    }else{
         setSuccessMsg(errorMsg)
         return true
     }
 }
 
-function submitDateCheck(){
-    let errorMsg = document.getElementById('dateErrorMsg');
-    let dates = [dia,mes,año]
-    let errorDates = []
-    dates.forEach((date)=>{
-        if(date.value === ""){
-            if (!errorDates.length){
-                errorDates = errorDates + " " + date.id
-            }else{
-                errorDates = errorDates + ", " + date.id
-            }
-        }
-    })
 
-    let error = "El campo " + errorDates + " esta vacio"
-    if(dia.value === "" || mes.value === "" || año.value === ""){
-        setErrorMsg(errorMsg ,error)
-    }else{
-        setSuccessMsg(errorMsg)
-    }
-}
-
-function submitCheckboxCheck(input){
-    let errorMsg = document.getElementById('checkboxErrorMsg');
-    console.log(input.value)
-}
 
 function blurPasswordRepeat(input){
-    let errorMsg = input.parentElement.children[2]
-    let passwordErrorMsg = contraseña.parentElement.children[2]
-    if(input.value === contraseña.value && input.value != "" && contraseñaRGEX.test(input.value)){
+    let errorMsg = document.querySelector('small#repetirContraseña');
+    input.classList.remove("input-neutral-selected")
+    input.classList.remove("input-error-selected")
+    if(input.value === ""){
+        setErrorClass(input)
+        setErrorMsg(errorMsg ,vacioError)
+        return false
+    }else if(input.value === contraseña.value && input.value != "" && contraseñaRGEX.test(input.value)){
         setSuccessClass(input)
         setSuccessMsg(errorMsg)
         return true
     }else{
         setErrorMsg(errorMsg ,repetirContraseñaError)
         setErrorClass(input)
-        setErrorMsg(passwordErrorMsg ,repetirContraseñaError)
-        setErrorClass(contraseña)
         return false
     }
 }
@@ -208,6 +241,8 @@ contraseña.addEventListener("blur", function( event ) {
     blurTextInput(event.target, contraseñaError, contraseñaRGEX)
 }, true);
 
+////////////////////////////////// repetir contraseña ////////////////////////////////////
+
 repetirContraseña.addEventListener("focus", function(event){
     focusInput(event.target)
 }, true);
@@ -216,22 +251,25 @@ repetirContraseña.addEventListener("blur", function( event ) {
     blurPasswordRepeat(event.target)
 }, true);
 
+
+////////////////////////////////// submit ////////////////////////////////////
+
 form.addEventListener("submit", function( submit ) {
+    submit.preventDefault()
     nombreResultado = blurTextInput(nombre, soloLetrasError, soloLetrasRGEX)
     apellidoResultado = blurTextInput(apellido, soloLetrasError, soloLetrasRGEX)
     diaResultado = blurDateInput(dia)
     mesResultado = blurDateInput(mes)
     añoResultado = blurDateInput(año)
-    submitDateCheck()
     emailResultado = blurTextInput(email, emailError, emailRGEX)
     contraseñaResultado = blurTextInput(contraseña, contraseñaError, contraseñaRGEX)
     repetirContraseñaResultado = blurPasswordRepeat(repetirContraseña)
-    submitCheckboxCheck(aceptarTyC)
-    if(nombreResultado && apellidoResultado && diaResultado && mesResultado && añoResultado &&  emailResultado && contraseñaResultado && repetirContraseñaResultado){
+    aceptarTyCResultado = submitCheckboxCheck(aceptarTyC, aceptarTyCError)
+    console.log(aceptarTyCResultado)
+    if(nombreResultado && apellidoResultado && diaResultado && mesResultado && añoResultado &&  emailResultado && contraseñaResultado && repetirContraseñaResultado && aceptarTyCResultado){
         form.submit()
     }else{
         submit.preventDefault()
-
     }
 
 }, true);
