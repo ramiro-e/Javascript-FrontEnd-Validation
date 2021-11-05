711edc066c829ead64a327bfc0001b35ef4f015f
ramiro-e <ramiroestevez96@gmail.com>
2021-11-05 14:24:15 -0300

    Instalando express validator / session

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let session = require("express-session")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/indexRoute');
+var usersRouter = require('./routes/usersRoute');
 
 var app = express();
 
@@ -13,11 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:"Javascript FrontEnd Validation"}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
