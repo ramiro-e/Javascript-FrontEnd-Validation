ae1565e40d1074cd517d835cc3e8737d025a6752
ramiro-e <ramiroestevez96@gmail.com>
2021-11-21 19:52:09 -0300

    Emprolijando codigo

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,54 +1,62 @@
+let form = document.querySelector('form')
 let email = document.querySelector('input#email')
 let contraseña = document.querySelector('input#contraseña')
 
+////////////////////////////////// expresiones regulares ////////////////////////////////////
+
 let emailRGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+////////////////////////////////// mensajes de error ////////////////////////////////////
 
 let vacioError = "El campo esta vacio"
 let emailError = "El email introducido es invalido"
 let contraseñaError = "El email o la contraseña intrducidos son invalidos"
+let usuarioError = "La combinacion de email y contraseña no es correcta"
+
+////////////////////////////////// eventos en focus ////////////////////////////////////
 
 function setErrorClass(input){
-    input.classList.remove("input-success")
-    input.classList.add("input-error")
+    input.classList.remove('input-success')
+    input.classList.add('input-error')
 }
 
 function setErrorMsg(errorMsg, error){
-    errorMsg.classList.remove("hide-error-msg")
+    errorMsg.classList.remove('hide-error-msg')
     errorMsg.innerText = error
 }
 
-function setSuccessClass(input){
-    input.classList.remove("input-error")
-    input.classList.add("input-success")
-}
-
 function setSuccessMsg(errorMsg){
-    errorMsg.classList.add("hide-error-msg")
+    errorMsg.classList.add('hide-error-msg')
 }
 
 function setSelect(input){
-    input.classList.remove("input-success")
-    input.classList.add("input-neutral-selected")
+    input.classList.remove('input-success')
+    input.classList.add('input-neutral-selected')
 }
 
 function setSelectError(input){
-    input.classList.remove("input-error")
-    input.classList.add("input-error-selected")
+    input.classList.remove('input-error')
+    input.classList.add('input-error-selected')
+}
+
+function removeSelected(input) {
+    input.classList.remove('input-neutral-selected')
+    input.classList.remove('input-error-selected')
 }
 
 function focusInput(input){
-    if(input
-        .classList.contains("input-error")){
+    if(input.classList.contains('input-error')){
         setSelectError(input)
     }else{
         setSelect(input)
     }
 }
 
+////////////////////////////////// eventos en blur ////////////////////////////////////
+
 function blurEmail(){
-    let errorMsg = document.querySelector("small#email");
-    email.classList.remove("input-neutral-selected")
-    email.classList.remove("input-error-selected")
+    let errorMsg = document.querySelector('small#email');
+
     if(email.value === ""){
         setErrorClass(email)
         setErrorMsg(errorMsg ,vacioError)
@@ -58,28 +66,93 @@ function blurEmail(){
         setErrorMsg(errorMsg, emailError)
         return false
     }else{
-        setSuccessClass(email)
         setSuccessMsg(errorMsg)
         return true
     }
 }
 
+function blurContraseña(){
+    let errorMsg = document.querySelector('small#contraseña');
+
+    if(contraseña.value === ""){
+        setErrorClass(contraseña)
+        setErrorMsg(errorMsg ,vacioError)
+        return false
+    }else{
+        setSuccessMsg(errorMsg)
+        return true
+    }
+}
+
+////////////////////////////////// eventos en submit ////////////////////////////////////
+
+async function checkContraseña(){
+    let errorMsg = document.querySelector('small#contraseña');
+    let emailExistsResult = await checkForm()
+    if(contraseña.value === ""){
+        setErrorClass(contraseña)
+        setErrorMsg(errorMsg ,vacioError)
+        return false
+    }else if (emailExistsResult == false){
+        setErrorMsg(errorMsg, usuarioError)
+        return false
+    }else if (emailExistsResult == true){
+        setSuccessMsg(errorMsg)
+        return true
+    }else{
+        return true
+    }
+
+}
+
+async function checkForm() {
+    let currentForm = JSON.stringify({password: contraseña.value, email: email.value})
+    let response = await fetchForm(currentForm)
+    let data = await response.json();
+    let checkLogin = data.checkLogin
+    return checkLogin
+}
+
+async function fetchForm(form) {
+    let response = await fetch('http://localhost:3000/users/checkLogin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: form
+    })
+    return response;
+}
+
+
 ////////////////////////////////// email ////////////////////////////////////
 
-email.addEventListener("focus", function(event){
+email.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-email.addEventListener("blur", function( event ) {
+email.addEventListener('blur', function( event ) {
+    removeSelected(event.target)
     blurEmail()
 }, true);
 
 ////////////////////////////////// contraseña ////////////////////////////////////
 
-contraseña.addEventListener("focus", function(event){
+contraseña.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-contraseña.addEventListener("blur", function( event ) {
-    blurTextInput(event.target, contraseñaError, contraseñaRGEX)
+contraseña.addEventListener('blur', function( event ) {
+    removeSelected(event.target)
+    blurContraseña()
 }, true);
+
+////////////////////////////////// submit ////////////////////////////////////
+
+form.addEventListener('submit',async function (submit) {
+    submit.preventDefault()
+    checkSubmitResultado = await checkContraseña()
+    if (checkSubmitResultado) {
+        form.submit()
+    }
+})
--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -9,10 +9,14 @@ let contraseña = document.querySelector('input#contraseña')
 let repetirContraseña = document.querySelector('input#repetirContraseña');
 let aceptarTyC = document.querySelector('input#aceptarTyC');
 
+////////////////////////////////// expresiones regulares ////////////////////////////////////
+
 let soloLetrasRGEX = /^[a-zA-Z\s]*$/;
 let fechaRGEX = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
 let emailRGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 let contraseñaRGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+////////////////////////////////// mensajes de error ////////////////////////////////////
 
 let vacioError = "El campo esta vacio"
 let soloLetrasError = "El campo solo puede contener letras"
@@ -21,6 +25,8 @@ let emailExistError = "El email ingresado ya esta registrado"
 let contraseñaError = "La contraseña contener letras mayusculas, minusculas y numeros "
 let repetirContraseñaError = "Las contraseñas no coinciden"
 let aceptarTyCError = "Debe aceptar los terminos y condiciones"
+
+////////////////////////////////// eventos en focus ////////////////////////////////////
 
 function setErrorClass(input){
     input.classList.remove("input-success")
@@ -51,6 +57,11 @@ function setSelectError(input){
     input.classList.add("input-error-selected")
 }
 
+function removeSelected(input) {
+    input.classList.remove('input-neutral-selected')
+    input.classList.remove('input-error-selected')
+}
+
 function focusInput(input){
     if(input.classList.contains("input-error")){
         setSelectError(input)
@@ -59,11 +70,12 @@ function focusInput(input){
     }
 }
 
+////////////////////////////////// eventos en blur y submit////////////////////////////////////
+
 function blurTextInput(input, error, RGEX){
     let errorMsgId = "small#" + input.id
     let errorMsg = document.querySelector(errorMsgId);
-    input.classList.remove("input-neutral-selected")
-    input.classList.remove("input-error-selected")
+    removeSelected(input)
     if(input.value === ""){
         setErrorClass(input)
         setErrorMsg(errorMsg ,vacioError)
@@ -81,8 +93,7 @@ function blurTextInput(input, error, RGEX){
 
 function blurDateInput(input){
     let errorMsg = document.querySelector('small#date')
-    input.classList.remove("input-neutral-selected")
-    input.classList.remove("input-error-selected")
+    removeSelected(input)
     let dates = [dia,mes,año]
     let errorDates = []
     let datePosition = dates.indexOf(input)
@@ -117,7 +128,7 @@ function checkInputFecha() {
     }
 }
 
-const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 (function populateMonths(){
     for(let i = 0; i < months.length; i++){
@@ -125,7 +136,7 @@ const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 
         option.textContent = months[i];
         let value = i + 1
         if(i<9){
-            option.value = "0" + value;
+            option.value = '0' + value;
         }else{
             option.value = value;
         }
@@ -154,16 +165,16 @@ function populateDays(mesNum){
             diaNum = 28;
         }
     }
-    const mesOption = document.createElement("option");
+    const mesOption = document.createElement('option');
     mesOption.textContent = "Dia";
-    mesOption.value = ""
+    mesOption.value = ''
     dia.appendChild(mesOption);
     for(let i = 1; i <= diaNum; i++){
-        const option = document.createElement("option");
+        const option = document.createElement('option');
         option.textContent = i;
         let value = i + 1
         if(i<9){
-            option.value = "0" + value;
+            option.value = '0' + value;
         }else{
             option.value = value;
         }
@@ -171,13 +182,13 @@ function populateDays(mesNum){
     }
     if(diaAnterior){
         dia.value = diaAnterior;
-        if(dia.value === ""){
+        if(dia.value === ''){
             dia.value = diaAnterior - 1;
         }
-        if(dia.value === ""){
+        if(dia.value === ''){
             dia.value = diaAnterior - 2;
         }
-        if(dia.value === ""){
+        if(dia.value === ''){
             dia.value = diaAnterior - 3;
         }
     }
@@ -186,7 +197,7 @@ function populateDays(mesNum){
 function populateYears(){
     let añoNum = new Date().getFullYear();
     for(let i = 0; i < 101; i++){
-        const option = document.createElement("option");
+        const option = document.createElement('option');
         option.textContent = añoNum - i;
         option.value = añoNum - i;
         año.appendChild(option);
@@ -212,8 +223,7 @@ function submitCheckboxCheck(input, error){
 
 function blurPasswordRepeat(input){
     let errorMsg = document.querySelector('small#repetirContraseña');
-    input.classList.remove("input-neutral-selected")
-    input.classList.remove("input-error-selected")
+    removeSelected(input)
     if(input.value === ""){
         setErrorClass(input)
         setErrorMsg(errorMsg ,vacioError)
@@ -231,8 +241,7 @@ function blurPasswordRepeat(input){
 
 async function blurEmailInput(){
     let errorMsg = document.querySelector('small#email');
-    email.classList.remove("input-neutral-selected")
-    email.classList.remove("input-error-selected")
+    removeSelected(email)
     let emailExistResult = await checkIfEmailExists()
     if(email.value === ""){
         setErrorClass(email)
@@ -278,105 +287,108 @@ async function fetchEmail(email) {
 
 ////////////////////////////////// nombre ////////////////////////////////////
 
-nombre.addEventListener("focus", function( event ) {
+nombre.addEventListener('focus', function( event ) {
     focusInput(event.target)
 }, true);
 
-nombre.addEventListener("blur", function( event ) {
+nombre.addEventListener('blur', function( event ) {
     blurTextInput(event.target, soloLetrasError, soloLetrasRGEX)
 }, true);
 
 ////////////////////////////////// apellido ////////////////////////////////////
 
-apellido.addEventListener("focus", function(event){
+apellido.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-apellido.addEventListener("blur", function( event ) {
+apellido.addEventListener('blur', function( event ) {
     blurTextInput(event.target, soloLetrasError, soloLetrasRGEX)
 }, true);
 
 ////////////////////////////////// dia ////////////////////////////////////
 
-dia.addEventListener("focus", function(event){
+dia.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-dia.addEventListener("blur", function( event ) {
+dia.addEventListener('blur', function( event ) {
     blurDateInput(event.target)
 }, true);
 
-dia.addEventListener("change", function( event ) {
+dia.addEventListener('change', function( event ) {
+    blurDateInput(event.target)
     diaAnterior = dia.value;
 }, true);
 
 ////////////////////////////////// mes ////////////////////////////////////
 
-mes.addEventListener("focus", function(event){
+mes.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-mes.addEventListener("blur", function( event ) { 
+mes.addEventListener('blur', function( event ) { 
     blurDateInput(event.target)
 }, true);
 
-mes.addEventListener("change", function( event ) { 
+mes.addEventListener('change', function( event ) {
+    blurDateInput(event.target) 
     populateDays(mes.value);
 }, true);
 
 ////////////////////////////////// año ////////////////////////////////////
 
-año.addEventListener("focus", function(event){
+año.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-año.addEventListener("blur", function( event ) {
+año.addEventListener('blur', function( event ) {
     blurDateInput(event.target)
 }, true);
 
-año.addEventListener("change", function( event ) {
+año.addEventListener('change', function( event ) {
+    blurDateInput(event.target)
     populateDays(mes.value);
 }, true);
 
 ////////////////////////////////// email ////////////////////////////////////
 
-email.addEventListener("focus", function(event){
+email.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-email.addEventListener("blur", function( event ) {
+email.addEventListener('blur', function( event ) {
     blurEmailInput()
 }, true);
 
 ////////////////////////////////// contraseña ////////////////////////////////////
 
-contraseña.addEventListener("focus", function(event){
+contraseña.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-contraseña.addEventListener("blur", function( event ) {
+contraseña.addEventListener('blur', function( event ) {
     blurTextInput(event.target, contraseñaError, contraseñaRGEX)
 }, true);
 
 ////////////////////////////////// repetir contraseña ////////////////////////////////////
 
-repetirContraseña.addEventListener("focus", function(event){
+repetirContraseña.addEventListener('focus', function(event){
     focusInput(event.target)
 }, true);
 
-repetirContraseña.addEventListener("blur", function( event ) {
+repetirContraseña.addEventListener('blur', function( event ) {
     blurPasswordRepeat(event.target)
 }, true);
 
 ////////////////////////////////// aceptarTyC ////////////////////////////////////
 
-aceptarTyC.addEventListener("change", function( event ) {
+aceptarTyC.addEventListener('change', function( event ) {
     submitCheckboxCheck(aceptarTyC, aceptarTyCError)
 }, true);
 
 ////////////////////////////////// submit ////////////////////////////////////
 
-form.addEventListener("submit", async function( submit ) {
+form.addEventListener('submit', async function( submit ) {
     submit.preventDefault()
     nombreResultado = blurTextInput(nombre, soloLetrasError, soloLetrasRGEX)
     apellidoResultado = blurTextInput(apellido, soloLetrasError, soloLetrasRGEX)
